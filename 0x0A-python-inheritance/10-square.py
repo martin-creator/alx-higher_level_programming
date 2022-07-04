cb4435632aed72841d219002e22ea799704f8825
martin-creator <martinlubowa@outlook.com>
2022-07-04 16:42:56 +0300

    a class Square that inherits from Rectangle

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+10-rectangle.py
+a class Rectangle that inherits from Rectangle (9-rectangle.py)
+add calculate area and size
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """calculate size and validate with integer validator"""
+    def __init__(self, size):
+        """find size and validate it as well"""
+        self.__size = size
+        self.integer_validator("size", size)
+        super().__init__(size, size)
